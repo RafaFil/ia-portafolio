7c30f44ae0b80fe4a9cc6d86c4cbaa97418f704c
RafaFil <filardisafa@gmail.com>
2023-08-26 16:22:52 -0300

    ciclo 5

--- src/app/modules/pages/main/main-page/main-page.component.ts
+++ src/app/modules/pages/main/main-page/main-page.component.ts
@@ -2,12 +2,18 @@ import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular
 import { Article } from 'src/app/modules/core/interfaces/article';
 import { ArticleService } from 'src/app/modules/core/services/article.service';
 import { fadeInUpAnimation } from '../animations/animationFadeIn';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'ia-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
-  //animations: [ fadeInUpAnimation ],
+  animations: [  trigger('fadeInOut', [
+      state('void', style({ opacity: 0, transform: 'translateY(20px)' })),
+      state('*', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition(':enter, :leave', animate('1s ease')),
+    ])],
 })
 export class MainPageComponent implements OnInit {
 
