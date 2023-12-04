dc0d3bd1e31b7277f76fcdfa185b6c115f1ecdf0
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2023-12-04 20:48:52 -0300

    added social

--- src/app/modules/pages/about/linkedin-pages/linkedin-pages.component.ts
+++ src/app/modules/pages/about/linkedin-pages/linkedin-pages.component.ts
@@ -13,8 +13,8 @@ export class LinkedinPagesComponent implements OnInit {
       name: "Santiago Alonso",
       // linkedinPfp: "https://media.licdn.com/dms/image/D4E35AQGEhGQsDzCFgA/profile-framedphoto-shrink_800_800/0/1695324450707?e=1700604000&v=beta&t=yMQdCGEz6rbeWWSooU0Ndwki43-1q0W_RuT4tu3JTew",
       // linkedinBackgroudn: "https://media.licdn.com/dms/image/D4D16AQEA1HIIxO-j5g/profile-displaybackgroundimage-shrink_350_1400/0/1685401764507?e=1705536000&v=beta&t=tW9UFonEsaSfBIa1aydTLqLAXKL6fDqrcZo9RVvV4Bk",
-      linkedin:"",
-      github:"",
+      linkedin:"https://www.linkedin.com/in/salonso1602/",
+      github:"https://github.com/Salonso1602",
       linkedinPfp: "",
       linkedinBackgroudn: ""
     },
@@ -22,28 +22,28 @@ export class LinkedinPagesComponent implements OnInit {
       name: "Rafael Filardi",
       linkedinPfp: "",
       linkedinBackgroudn: "",
-      linkedin:"",
-      github:""
+      linkedin:"https://www.linkedin.com/in/rafael-filardi/",
+      github:"https://github.com/RafaFil"
     },
     {
       name: "Joaquin Gasco",
       linkedinPfp: "",
       linkedinBackgroudn: "",
-      linkedin:"",
-      github:""
+      linkedin:"https://www.linkedin.com/in/joaquingasco/",
+      github:"https://github.com/JoacoG11"
     },    {
       name: "Antonella Gutierrez",
       linkedinPfp: "",
       linkedinBackgroudn: "",
-      linkedin:"",
-      github:""
+      linkedin:"https://www.linkedin.com/in/antonellagutierrezsagrera/",
+      github:"https://github.com/antomgutierrez"
     },
     {
       name: "Juan Manuel Perez",
       linkedinPfp: "",
       linkedinBackgroudn: "",
-      linkedin:"",
-      github:""
+      linkedin:"https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUQbmV2ZXIgZ29ubmEgZ2l2ZQ%3D%3D",
+      github:"https://github.com/jumpert"
     }
   ];
 
